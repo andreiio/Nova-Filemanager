2bb9f77e714ec9c27ba97cfd86bd1d2130b79c17
Andrei Ioniță <hi@andrei.io>
2021-10-07 14:16:57 +0100

    wip

--- resources/js/field.js
+++ resources/js/field.js
@@ -1,5 +1,16 @@
+import VueLazyload from 'vue-lazyload';
+
 Nova.booting(Vue => {
     Vue.component('index-filemanager-field', require('./field/IndexField'));
     Vue.component('detail-filemanager-field', require('./field/DetailField'));
     Vue.component('form-filemanager-field', require('./field/FormField'));
+
+    Vue.use(VueLazyload, {
+        lazyComponent: true,
+        observer: true,
+        observerOptions: {
+            rootMargin: '0px',
+            threshold: 1.0,
+        },
+    });
 });
